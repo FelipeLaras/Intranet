96322b96ba65aba9143b99bbd797462fb5abb26e
Felipe Lara <69820156+FelipeLaras@users.noreply.github.com>
2020-09-17 10:25:22 -0300

    Novo Favicon
    Index, alterado titulo

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Intranet Grupo Servopa</title>
+    <title>Intranet Rede Paranapart</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta name="keywords">
     <meta name="description">
